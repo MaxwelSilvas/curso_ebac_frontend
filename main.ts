10098cc919fa5c0394977f95d20e116cda0c64b1
Maxwel Silva <108342352+MaxwelSilvas@users.noreply.github.com>
2024-05-16 13:55:47 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,13 @@
+function multiplicaNumeros(numeroA: number, numeroB: number): number {
+    return numeroA * numeroB;
+}
+
+const resultadoMutiplicacao = multiplicaNumeros(10, 5);
+console.log(resultadoMutiplicacao);
+
+function saudacao(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+const resultadoNome = saudacao("Maxwel");
+console.log(resultadoNome);
